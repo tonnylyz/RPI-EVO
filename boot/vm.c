9d5d63173843c8ff82bcbed799da9bf2c450dc58
tonnylyz <lyztonny@gmail.com>
2018-02-04 22:49:46 +0800

    refine driver

--- boot/vm.c
+++ boot/vm.c
@@ -1,4 +1,5 @@
 #include <mmu.h>
+
 u_long freemem;
 
 void boot_bzero(void *b, size_t len) {
@@ -55,136 +56,78 @@ void boot_map_segment(Pde *pgdir, u_long va, u_long size, u_long pa, int perm) {
     }
 }
 
+volatile unsigned int __attribute__((aligned(16))) mbox[36];
 
-#define MMIO_BASE       0x3F000000
-
-#define GPFSEL0         ((volatile unsigned int*)(MMIO_BASE+0x00200000))
-#define GPFSEL1         ((volatile unsigned int*)(MMIO_BASE+0x00200004))
-#define GPFSEL2         ((volatile unsigned int*)(MMIO_BASE+0x00200008))
-#define GPFSEL3         ((volatile unsigned int*)(MMIO_BASE+0x0020000C))
-#define GPFSEL4         ((volatile unsigned int*)(MMIO_BASE+0x00200010))
-#define GPFSEL5         ((volatile unsigned int*)(MMIO_BASE+0x00200014))
-#define GPSET0          ((volatile unsigned int*)(MMIO_BASE+0x0020001C))
-#define GPSET1          ((volatile unsigned int*)(MMIO_BASE+0x00200020))
-#define GPCLR0          ((volatile unsigned int*)(MMIO_BASE+0x00200028))
-#define GPLEV0          ((volatile unsigned int*)(MMIO_BASE+0x00200034))
-#define GPLEV1          ((volatile unsigned int*)(MMIO_BASE+0x00200038))
-#define GPEDS0          ((volatile unsigned int*)(MMIO_BASE+0x00200040))
-#define GPEDS1          ((volatile unsigned int*)(MMIO_BASE+0x00200044))
-#define GPHEN0          ((volatile unsigned int*)(MMIO_BASE+0x00200064))
-#define GPHEN1          ((volatile unsigned int*)(MMIO_BASE+0x00200068))
-#define GPPUD           ((volatile unsigned int*)(MMIO_BASE+0x00200094))
-#define GPPUDCLK0       ((volatile unsigned int*)(MMIO_BASE+0x00200098))
-#define GPPUDCLK1       ((volatile unsigned int*)(MMIO_BASE+0x0020009C))
-
-#define UART0_DR        ((volatile unsigned int*)(MMIO_BASE+0x00201000))
-#define UART0_FR        ((volatile unsigned int*)(MMIO_BASE+0x00201018))
-#define UART0_IBRD      ((volatile unsigned int*)(MMIO_BASE+0x00201024))
-#define UART0_FBRD      ((volatile unsigned int*)(MMIO_BASE+0x00201028))
-#define UART0_LCRH      ((volatile unsigned int*)(MMIO_BASE+0x0020102C))
-#define UART0_CR        ((volatile unsigned int*)(MMIO_BASE+0x00201030))
-#define UART0_IMSC      ((volatile unsigned int*)(MMIO_BASE+0x00201038))
-#define UART0_ICR       ((volatile unsigned int*)(MMIO_BASE+0x00201044))
-
-
-/* mailbox message buffer */
-volatile unsigned int  __attribute__((aligned(16))) mbox[36];
-
-#define VIDEOCORE_MBOX  (MMIO_BASE+0x0000B880)
-#define MBOX_READ       ((volatile unsigned int*)(VIDEOCORE_MBOX+0x0))
-#define MBOX_POLL       ((volatile unsigned int*)(VIDEOCORE_MBOX+0x10))
-#define MBOX_SENDER     ((volatile unsigned int*)(VIDEOCORE_MBOX+0x14))
-#define MBOX_STATUS     ((volatile unsigned int*)(VIDEOCORE_MBOX+0x18))
-#define MBOX_CONFIG     ((volatile unsigned int*)(VIDEOCORE_MBOX+0x1C))
-#define MBOX_WRITE      ((volatile unsigned int*)(VIDEOCORE_MBOX+0x20))
-#define MBOX_RESPONSE   0x80000000
-#define MBOX_FULL       0x80000000
-#define MBOX_EMPTY      0x40000000
-
-#define MBOX_REQUEST    0
-
-/* channels */
-#define MBOX_CH_POWER   0
-#define MBOX_CH_FB      1
-#define MBOX_CH_VUART   2
-#define MBOX_CH_VCHIQ   3
-#define MBOX_CH_LEDS    4
-#define MBOX_CH_BTNS    5
-#define MBOX_CH_TOUCH   6
-#define MBOX_CH_COUNT   7
-#define MBOX_CH_PROP    8
-
-/* tags */
-#define MBOX_TAG_GETSERIAL      0x10004
-#define MBOX_TAG_SETCLKRATE     0x38002
-#define MBOX_TAG_LAST           0
-
-int mbox_call(unsigned char ch)
-{
+int mbox_call(unsigned char ch) {
+#define MBOX_READ       ((volatile u_int*)(0x3F00B880))
+#define MBOX_STATUS     ((volatile u_int*)(0x3F00B898))
+#define MBOX_WRITE      ((volatile u_int*)(0x3F00B8A0))
     unsigned int r;
-    /* wait until we can write to the mailbox */
-    do{asm volatile("nop");}while(*MBOX_STATUS & MBOX_FULL);
-    /* write the address of our message to the mailbox with channel identifier */
-    *MBOX_WRITE = (((unsigned int)((unsigned long)&mbox)&~0xF) | (ch&0xF));
-    /* now wait for the response */
-    while(1) {
-        /* is there a response? */
-        do{asm volatile("nop");}while(*MBOX_STATUS & MBOX_EMPTY);
-        r=*MBOX_READ;
-        /* is it a response to our message? */
-        if((unsigned char)(r&0xF)==ch && (r&~0xF)==(unsigned int)((unsigned long)&mbox))
-            /* is it a valid successful response? */
-            return mbox[1]==MBOX_RESPONSE;
+    do { asm volatile("nop"); } while (*MBOX_STATUS & 0x80000000);
+    *MBOX_WRITE = (((unsigned int) ((unsigned long) &mbox) & ~0xF) | (ch & 0xF));
+    while (1) {
+        do { asm volatile("nop"); } while (*MBOX_STATUS & 0x40000000);
+        r = *MBOX_READ;
+        if ((unsigned char) (r & 0xF) == ch && (r & ~0xF) == (unsigned int) ((unsigned long) &mbox))
+            return mbox[1] == 0x80000000;
     }
-    return 0;
 }
 
-void uart_init()
-{
+void uart_init() {
+#define UART0_IBRD      ((volatile u_int*)(0x3F201024))
+#define UART0_FBRD      ((volatile u_int*)(0x3F201028))
+#define UART0_LCRH      ((volatile u_int*)(0x3F20102C))
+#define UART0_CR        ((volatile u_int*)(0x3F201030))
+#define UART0_ICR       ((volatile u_int*)(0x3F201044))
+#define GPFSEL1         ((volatile u_int*)(0x3F200004))
+#define GPPUD           ((volatile u_int*)(0x3F200094))
+#define GPPUDCLK0       ((volatile u_int*)(0x3F200098))
+#define UART0_DR        ((volatile u_int*)(0x3F201000))
+#define UART0_FR        ((volatile u_int*)(0x3F201018))
     register unsigned int r;
     *UART0_CR = 0;
-    mbox[0] = 8*4;
-    mbox[1] = MBOX_REQUEST;
-    mbox[2] = MBOX_TAG_SETCLKRATE; // set clock rate
+    mbox[0] = 8 * 4;
+    mbox[1] = 0;
+    mbox[2] = 0x38002; // set clock rate
     mbox[3] = 12;
     mbox[4] = 8;
     mbox[5] = 2;           // UART clock
     mbox[6] = 4000000;     // 4Mhz
-    mbox[7] = MBOX_TAG_LAST;
-    mbox_call(MBOX_CH_PROP);
-
-    r=*GPFSEL1;
-    r&=~((7<<12)|(7<<15)); // gpio14, gpio15
-    r|=(4<<12)|(4<<15);    // alt0
+    mbox[7] = 0;
+    mbox_call(8);
+    r = *GPFSEL1;
+    r &= ~((7 << 12) | (7 << 15)); // gpio14, gpio15
+    r |= (4 << 12) | (4 << 15);    // alt0
     *GPFSEL1 = r;
     *GPPUD = 0;            // enable pins 14 and 15
-    r=150; while(r--) { asm volatile("nop"); }
-    *GPPUDCLK0 = (1<<14)|(1<<15);
-    r=150; while(r--) { asm volatile("nop"); }
+    r = 150;
+    while (r--) { asm volatile("nop"); }
+    *GPPUDCLK0 = (1 << 14) | (1 << 15);
+    r = 150;
+    while (r--) { asm volatile("nop"); }
     *GPPUDCLK0 = 0;        // flush GPIO setup
-
     *UART0_ICR = 0x7FF;    // clear interrupts
     *UART0_IBRD = 2;       // 115200 baud
     *UART0_FBRD = 0xB;
-    *UART0_LCRH = 0b11<<5; // 8n1
+    *UART0_LCRH = 0b11 << 5; // 8n1
     *UART0_CR = 0x301;     // enable Tx, Rx, FIFO
 }
 
 void uart_send(unsigned int c) {
-    do{asm volatile("nop");}while(*UART0_FR&0x20);
-    *UART0_DR=c;
+    do { asm volatile("nop"); } while (*UART0_FR & 0x20);
+    *UART0_DR = c;
 }
 
 char uart_getc() {
     char r;
-    do{asm volatile("nop");}while(*UART0_FR&0x10);
-    r=(char)(*UART0_DR);
-    return r=='\r'?'\n':r;
+    do { asm volatile("nop"); } while (*UART0_FR & 0x10);
+    r = (char) (*UART0_DR);
+    return r == '\r' ? '\n' : r;
 }
 
 void uart_puts(char *s) {
-    while(*s) {
-        if(*s=='\n')
+    while (*s) {
+        if (*s == '\n')
             uart_send('\r');
         uart_send(*s++);
     }
@@ -193,11 +136,11 @@ void uart_puts(char *s) {
 void uart_hex(unsigned int d) {
     unsigned int n;
     int c;
-    for(c=28;c>=0;c-=4) {
+    for (c = 28; c >= 0; c -= 4) {
         // get highest tetrad
-        n=(d>>c)&0xF;
+        n = (d >> c) & 0xF;
         // 0-9 => '0'-'9', 10-15 => 'A'-'F'
-        n+=n>9?0x37:0x30;
+        n += n > 9 ? 0x37 : 0x30;
         uart_send(n);
     }
 }
@@ -207,12 +150,12 @@ void vm_init() {
     Pde *pgdir;
     freemem = KERNEL_PGDIR_PA;
     pgdir = boot_alloc(BY2PG, BY2PG, 1);
-    
+
     // 0x00000000 - 0x3F000000 Normal memory
     boot_map_segment(pgdir, 0, MAXPA, 0, PTE_NORMAL | PTE_INNER_SHARE);
     // 0x3F000000 - 0x40000000 Device I/O
     boot_map_segment(pgdir, MAXPA, 0x01000000, MAXPA, PTE_DEVICE | PTE_OUTER_SHARE);
-    
+
     uart_puts("uart_init ok!\n");
     uart_puts("boot_map_segment ok!\n");
 }
