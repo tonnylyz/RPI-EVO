6fd01b2267e5b48afe64bac1be0b27a51edf8b4c
tonnylyz <lyztonny@gmail.com>
2018-03-01 20:40:47 +0800

    Reduce typedef

--- include/types.h
+++ include/types.h
@@ -1,24 +1,10 @@
 #ifndef OSLABPI_TYPE_H
 #define OSLABPI_TYPE_H
 
-typedef unsigned char u8;
-typedef	unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long u64;
-
-typedef unsigned long u_int64_t;
-typedef unsigned short u_int16_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef unsigned int u_int32_t;
-typedef signed long int64_t;
-
 typedef	unsigned char	u_char;
 typedef	unsigned short	u_short;
 typedef	unsigned int	u_int;
 typedef	unsigned long	u_long;
-
-typedef unsigned long long size_t;
 
 #ifndef NULL
 #define NULL ((void *) 0)
